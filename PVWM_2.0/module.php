0832ec981b4f640e1f7f2b55fc6b8367097b36c2
Sol-IoTiv <70333661+Sol-IoTiv@users.noreply.github.com>
2025-08-22 12:16:07 +0200

    dix

--- PVWM_2.0/module.php
+++ PVWM_2.0/module.php
@@ -10,7 +10,7 @@ class GoEMQTTMirror extends IPSModule
         $this->RegisterPropertyString('BaseTopic', 'go-eCharger/285450'); // <<< anpassen
 
         // Kern-Variablen
-        $this->RegisterVariableInteger('Ampere_A',         'Ampere [A]',     '~Intensity.Ampere', 10);
+        $this->RegisterVariableInteger('Ampere_A',         'Ampere [A]',     '', 10);
         $this->RegisterVariableInteger('Leistung_W',       'Leistung [W]',   '~Watt',             20);
         $this->RegisterVariableBoolean('FahrzeugVerbunden','Fahrzeug verbunden', '~Switch',        30);
         $this->RegisterVariableInteger('ALW',              'ALW (0/1)',      '',                  40);
